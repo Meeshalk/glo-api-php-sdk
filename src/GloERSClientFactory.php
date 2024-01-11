7342d3f94832828a589050628e1849f022aeb5eb
Meeshal kumar <meeshal1994@gmail.com>
2024-01-12 01:32:48 +0530

    feat: added client factory with logger

---
+++ src/GloERSClientFactory.php
@@ -0,0 +1,47 @@
+<?php
+
+    namespace Meeshalk\GloERSWebServiceSdk;
+
+    use GuzzleHttp\Client;
+    use Http\Client\Common\PluginClient;
+    use Meeshalk\GloERSWebServiceSdk\Logger\LoggerPlugin;
+    use Monolog\Handler\RotatingFileHandler;
+    use Monolog\Logger;
+    use Phpro\SoapClient\Caller\EngineCaller;
+    use Phpro\SoapClient\Caller\EventDispatchingCaller;
+    use Phpro\SoapClient\Soap\DefaultEngineFactory;
+    use Psr\Log\LoggerInterface;
+    use Soap\ExtSoapEngine\ExtSoapOptions;
+    use Soap\Psr18Transport\Psr18Transport;
+    use Symfony\Component\EventDispatcher\EventDispatcher;
+
+    class GloERSClientFactory
+    {
+
+        public static function factory(
+            string $wsdl, LoggerInterface $logger = null
+        ): GloERSClient {
+            if ($logger === null) {
+                $logger = new Logger('http',
+                    [new RotatingFileHandler('logs/mono.log')]);
+            }
+            $loggerPlugin = new LoggerPlugin($logger);
+
+            $transport = Psr18Transport::createForClient(
+                new PluginClient(new Client(), [$loggerPlugin])
+            );
+
+            $engine = DefaultEngineFactory::create(
+                ExtSoapOptions::defaults($wsdl, [])
+                    ->withClassMap(GloERSClassmap::getCollection()), $transport
+            );
+
+            $eventDispatcher = new EventDispatcher();
+            $caller          = new EventDispatchingCaller(new EngineCaller($engine),
+                $eventDispatcher);
+
+            return new GloERSClient($caller);
+        }
+
+    }
+
